8f00182783061226de36d3bcfc7c024aa098f42e
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 18:49:00 -0500

    Just redirect to it?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,15 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['../demo-styling.css'],
 })
 export class AppComponent {
+  constructor(private location: Location) {}
+
   authorizeWindowUrl = `https://api.instagram.com/oauth/authorize
     ?client_id=1861484840866990,
     &redirect_uri=https://oddwoods.netlify.app/auth,
     &response_type=code,
     &scope=user_profile,user_media`;
+
+  ngOnInit() {
+    this.location.go(this.authorizeWindowUrl);
+  }
 }
