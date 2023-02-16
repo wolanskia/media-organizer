e30bb379d0875155c032fb2cea5de616e9d695ee
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 18:05:54 -0500

    Ignore tests

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -1,17 +1,18 @@
-describe('empty spec', () => {
+xdescribe('empty spec', () => {
   beforeEach(() => {
-    cy.visit('/')
-  })
+    cy.visit('/');
+  });
 
   it('displays the resources text', () => {
-    cy.get('h2')
-    .contains('This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify');
-  })
+    cy.get('h2').contains(
+      'This is a bare-bones Angular project that has everything you need to quickly deploy it to Netlify'
+    );
+  });
   it('renders the image', () => {
     cy.get('img')
-    .should('be.visible')
-    .and(($img) => {
-      expect($img[0].naturalWidth).to.be.greaterThan(0);
-    })
-  })
-})
+      .should('be.visible')
+      .and(($img) => {
+        expect($img[0].naturalWidth).to.be.greaterThan(0);
+      });
+  });
+});
