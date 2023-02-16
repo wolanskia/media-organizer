cf8852f78da33910b9dff01c519d4be312471bf8
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 18:15:55 -0500

    Test authorization window

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['../demo-styling.css']
+  styleUrls: ['../demo-styling.css'],
 })
 export class AppComponent {
-  title = 'angular-quickstart';
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http
+      .get(
+        `GET https://api.instagram.com/oauth/authorize
+    ?client_id=1861484840866990,
+    &redirect_uri=https://oddwoods.netlify.app/auth/,
+    &response_type=code,
+    &scope=user_profile,user_media`
+      )
+      .subscribe();
+  }
 }
