fe0dda874971e1cd8948b4cfee0a1191941e81c9
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 18:54:58 -0500

    Fix authorize window url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,7 @@ import { Location } from '@angular/common';
 export class AppComponent {
   constructor(private location: Location) {}
 
-  authorizeWindowUrl = `https://api.instagram.com/oauth/authorize
-    ?client_id=1861484840866990,
-    &redirect_uri=https://oddwoods.netlify.app/auth,
-    &response_type=code,
-    &scope=user_profile,user_media`;
+  authorizeWindowUrl = `https://api.instagram.com/oauth/authorize?client_id=1861484840866990&redirect_uri=https://oddwoods.netlify.app/auth&response_type=code&scope=user_profile,user_media`;
 
   ngOnInit() {
     window.location.replace(this.authorizeWindowUrl);
