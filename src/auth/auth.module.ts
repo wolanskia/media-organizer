dc024908f8d0b14ef8eba2c0a424c90ee5a61b96
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 22:34:11 -0500

    Add RouterModule to imports

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { UserContextGuard } from './user-context.guard';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   declarations: [AuthComponent],
   providers: [UserContextGuard],
   exports: [AuthComponent],
