908649a64070cbb4df5cf49912d8852dbe05d8f4
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 21:14:49 -0500

    Missing import statement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Location } from '@angular/common';
 import { SessionService } from 'src/auth/session.service';
 
 @Component({
@@ -8,10 +7,7 @@ import { SessionService } from 'src/auth/session.service';
   styleUrls: ['../demo-styling.css'],
 })
 export class AppComponent {
-  constructor(
-    private location: Location,
-    private sessionService: SessionService
-  ) {}
+  constructor(private sessionService: SessionService) {}
 
   authorizeWindowUrl = `https://api.instagram.com/oauth/authorize?client_id=1254574731831152&redirect_uri=https://oddwoods.netlify.app/auth&response_type=code&scope=user_profile,user_media`;
 
