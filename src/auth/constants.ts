2cd7093d8a342547b31810ecb40a92cf5ac39908
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-16 23:13:45 -0500

    Set clientId to string

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,4 +1,4 @@
-export const clientId = 1254574731831152;
+export const clientId = '1254574731831152';
 export const redirectUri = 'https://oddwoods.netlify.app/auth';
 
 export interface UserSession {
