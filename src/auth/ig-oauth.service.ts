7be3aa08124a21a913381d1767ed960b962623c4
Alex Ricard <alexandria.ricard@gmail.com>
2023-02-18 08:31:05 -0500

    Revert to JSON body

--- src/auth/ig-oauth.service.ts
+++ src/auth/ig-oauth.service.ts
@@ -30,8 +30,8 @@ export class IgOAuthService {
         mode: 'no-cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
         headers: {
-          // 'Content-Type': 'application/json',
-          'Content-Type': 'application/x-www-form-urlencoded',
+          'Content-Type': 'application/json',
+          // 'Content-Type': 'application/x-www-form-urlencoded',
         },
         referrerPolicy: 'strict-origin-when-cross-origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
         body: JSON.stringify(body), // body data type must match "Content-Type" header
